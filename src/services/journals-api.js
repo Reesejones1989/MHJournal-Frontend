b80e43fd84320451790112ff44cc52e3de9c9613
Reesejones1989 <mauricejonesjr@gmail.com>
2023-10-11 21:33:37 -0400

    fixed bug

--- src/services/journals-api.js
+++ src/services/journals-api.js
@@ -1,23 +1,23 @@
 import axios from "axios"
 
 const baseURL = 'http://localhost:3005/journals'
-const baseURLRender = ''
+const baseURLRender = 'https://mhjounal-backend.onrender.com/journals'
 
 export const getJournals = async () => {
-    const URL = baseURL
+    const URL = baseURLRender || baseURL
     const response = await axios.get(URL)
     return response
 }
 
 export const getOneJournal = async (id) => {
-    const URL = `${baseURL}/${id}`
+    const URL = `${baseURLRender || baseURL}/${id}`
     const response = await axios.get(URL)
     console.log(response)
     return response
 }
 
 export const editJournal = async (id, data) => {
-    const URL = `${baseURL}/${id}`
+    const URL = `${baseURLRender || baseURL}/${id}`
     console.log('URL', URL)
     const response = await axios.put(URL, data)
     console.log(response)
@@ -26,63 +26,12 @@ export const editJournal = async (id, data) => {
 }
 
 export const createJournal = async (journal) => {
-    const URL = baseURL
+    const URL = baseURLRender || baseURL
     const response = await axios.post(URL, journal)
     return response
 }
 export const deleteJournal = async (id) => {
-    const URL = `${baseURL}/${id}`
+    const URL = `${baseURLRender || baseURL}/${id}`
     const response = axios.delete(URL)
     return response
 }
-
-
-
-
-// import { useEffect, useState } from "react";
-
-
-// export default function journalapi (props) {
-// //     const journalApiURL = 'http://localhost:3005/journals'
-    
-//     // const [journal, setJournal] = useState(null);
-
-
-// // //ALL JOURNALS
-// //      const getJournals = async () => {
-// //         const URL = journalApiURL
-// //         const response = await fetch(URL);
-// //         const data = await response.json();
-// //         setJournal (data);
-// //     };
-// //     useEffect(() => {
-// //         getJournals();
-// //         }, []);
-
-// //         const loaded = () => {
-// //             console.log(journal.title)
-// //             return (
-// //               <div>
-// //                 <h1>
-// //                 Title: {journal.title}
-// //                 </h1>
-// //                 <h2>
-// //                 Journal Entry: {journal.journalEntry}
-// //                 </h2>
-// //                 <h2>
-// //                Date: {journal.date}
-// //                 </h2>
-// //               </div>
-// //             );
-// //           };
-
-// //           const loading = () => {
-// //             return <h1>Loading...</h1>;
-// //            };
-
-
-// //      const getAffirms = () => {
-
-// // }
-// // return journal && journal ? loaded() : loading();
-// }
